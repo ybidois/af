30a21dd6f7f69dcb73c5169659ef82551e861c61
Yannick Bidois <YnotDream>
2020-06-04 14:03:43 +0200

    Change copywriting and images to reflect family focus

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ybidois/AmourFood/af/.cache/dev-404-page.js"))),
   "component---node-modules-arshad-gatsby-theme-page-core-src-templates-page-query-js": hot(preferDefault(require("/Users/ybidois/AmourFood/af/node_modules/@arshad/gatsby-theme-page-core/src/templates/page-query.js")))
 }
 
